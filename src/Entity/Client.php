bf25a3d8c31bcb7779761f266b04cd115d47678a
LazuliEXE <lazulitechexe@gmail.com>
2025-04-01 14:11:51 +0200

    Test git

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -116,15 +116,14 @@ class Client extends Personne
         return $this;
     }
 
-    public function removeAchatEffectue(Achat $achatEffectue): static
+        public function removeAchatEffectue(Achat $achatEffectue): static
     {
-        if ($this->achat_effectue->removeElement($achatEffectue)) {
+        if ($this->achat_effectue->removeElement($achatEffectue) && $achatEffectue->getAcheteur() === $this) {
             // set the owning side to null (unless already changed)
-            if ($achatEffectue->getAcheteur() === $this) {
-                $achatEffectue->setAcheteur(null);
-            }
+            $achatEffectue->setAcheteur(null);
         }
 
         return $this;
     }
+    
 }
