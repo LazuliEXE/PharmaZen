5831e9f91abc6a1d437806e09cdb8e5550e7660a
LazuliEXE <lazulitechexe@gmail.com>
2025-05-22 14:48:30 +0200

    Update MCD

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-composer install && php bin/console doctrine:database:create && php bin/console doctrine:migrations:migrate --no-interaction && php bin/console doctrine:fixtures:load --no-interaction --purge-with-truncate
+composer install && php bin/console doctrine:database:create && php bin/console doctrine:migrations:migrate --no-interaction && php bin/console doctrine:fixtures:load --no-interaction
